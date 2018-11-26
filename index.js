83f01fbc7091139cf8d9446127039e819f7bdfa7
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-11-26 09:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function dwarfRollCall(dwarves) {
+  var rollcall = "";
+  dwarves.forEach(function(name, i) {
+  	rollcall += (i+1 + ". " + name + " ");
+  });
+  return rollcall;
 }
+
+dwarfRollCall(["Doc", "Dopey", "Bashful", "Grumpy"]);
+
+
+
+
+
+dwarfRollCall(["Doc", "Dopey", "Bashful", "Grumpy"]);
+
+
 
 function summonCaptainPlanet(planeteerCalls){
 }
